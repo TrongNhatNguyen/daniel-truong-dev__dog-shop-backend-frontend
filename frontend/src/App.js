bf2b691b19de223a7bf45afef8993d84a6d0a276
Nguyen Trong Nhat <nguyentrongnhat240700@gmail.com>
2022-01-29 07:37:57 +0700

    init project

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="container">content</div>
+        </div>
+    );
+}
+
+export default App;
